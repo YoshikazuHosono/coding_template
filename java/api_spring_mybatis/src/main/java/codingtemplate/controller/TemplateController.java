cac2cc98a76d1023f7a4e4f4f0e9b3d0fc7cf383
YoshikazuHosono <yyhhwork@gmail.com>
2020-01-02 23:27:15 +0900

    トランザクション管理を追加

--- java/api_spring_mybatis/src/main/java/codingtemplate/controller/TemplateController.java
+++ java/api_spring_mybatis/src/main/java/codingtemplate/controller/TemplateController.java
@@ -4,6 +4,7 @@ import codingtemplate.model.TemplateModel;
 import codingtemplate.service.TemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class TemplateController {
     @Autowired
     private TemplateService service;
 
+    @Transactional
     @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<TemplateModel> getTemplate() throws Exception {
         return service.getTemplate();
